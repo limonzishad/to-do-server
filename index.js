7f440ed029cde05f65d417e19f98dddd93ae01ef
limonzishad <zishadlimon@gmail.com>
2022-05-19 02:29:38 +0600

    third commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fx3wy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fx3wy.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
@@ -17,22 +17,22 @@ async function run() {
         await client.connect();
         const itemCollection = client.db('to-do').collection('notes');
 
-        //items api
-        app.get('/items', async (req, res) => {
+        //notes api
+        app.get('/notes', async (req, res) => {
             const query = {};
             const cursor = itemCollection.find(query);
             const items = await cursor.toArray();
             res.send(items);
         });
 
-        //add new item
+        //add new note
         app.post('/note', async (req, res) => {
             const newItem = req.body;
             const result = await itemCollection.insertOne(newItem);
             res.send(result);
         });
 
-        //delete item
+        //delete note
         app.delete('/item/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
